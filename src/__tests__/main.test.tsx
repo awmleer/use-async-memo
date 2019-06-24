733352939df0f24157358c721f0290ff84997828
卡晨 <guangbo.hgb@antfin.com>
2019-06-24 15:18:12 +0800

    add test

--- src/__tests__/main.test.tsx
+++ src/__tests__/main.test.tsx
@@ -1,8 +1,29 @@
 import * as testing from '@testing-library/react'
-import {FC, useState} from 'react'
+import {FC} from 'react'
 import * as React from 'react'
 import {useAsyncMemo} from '../index'
 
-test('only create once', function () {
-  expect(1).toBe(1)
+test('get async data', async function () {
+  function getAsyncData() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(1)
+      }, 100)
+    })
+  }
+  
+  const App: FC = function (props) {
+    const data = useAsyncMemo(() => getAsyncData())
+    return (
+      <div>
+        {data}
+      </div>
+    )
+  }
+  const renderer = testing.render(
+    <App/>
+  )
+  expect(renderer.asFragment()).toMatchSnapshot()
+  await getAsyncData()
+  expect(renderer.asFragment()).toMatchSnapshot()
 })
