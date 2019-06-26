7fe09bbbed9813aebac0892c32b088aab656041c
卡晨 <guangbo.hgb@antfin.com>
2019-06-26 11:13:10 +0800

    add initial arg

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import {DependencyList, useEffect, useState} from 'react'
 
-export function useAsyncMemo<T>(factory: () => Promise<T>, deps?: DependencyList): T {
-  const [val, setVal] = useState<T>()
+export function useAsyncMemo<T>(factory: () => Promise<T>, deps: DependencyList, initial: T = undefined): T {
+  const [val, setVal] = useState<T>(initial)
   useEffect(() => {
     let cancel = false
     factory().then((val) => {
